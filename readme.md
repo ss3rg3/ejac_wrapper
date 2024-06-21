9a531f9dbf307c3173aae55cf188d5f40cc51d19
Klaus <no.mail.to.grab.x123@here.com>
2024-06-21 11:51:45 +0200

    init

---
+++ readme.md
@@ -0,0 +1,1 @@
+# ejac_helpers
---
+++ src/main/java/ejachelpers/mapping/Main.java
@@ -0,0 +1,8 @@
+package ejachelpers.mapping;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
